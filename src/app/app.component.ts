4a7ac62e4cd857e88833ba977bf30218a4f04fb5
BenWuerselen <benjamin.ipp@moser.de>
2021-01-21 19:28:06 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,61 @@ import { ODataClient, ODataServiceFactory } from "angular-odata";
 export class AppComponent {
   name = "Angular " + VERSION.major;
 
-  constructor(private factory: ODataServiceFactory) {}
+  constructor(private factory: ODataServiceFactory) {
+    this.queries();
+  }
+
+  queries() {
+    // Use OData Service Factory
+    let airportsService = this.factory.entity<Airport>("Airports");
+    let peopleService = this.factory.entity<Person>("People");
+
+    let airports = airportsService.entities();
+
+    // Fetch all airports
+    airports.all().subscribe(aports => console.log("All: ", aports));
+  }
+}
+
+export interface Person {
+  UserName: string;
+  FirstName: string;
+  LastName: string;
+  Emails?: string[];
+  Gender?: PersonGender;
+  Friends?: Person[];
+  Trips?: Trip[];
+  Photo?: Photo;
+}
+
+export interface Trip {
+  TripId: number;
+  ShareId?: string;
+  Description?: string;
+  Name: string;
+  Budget: number;
+  StartsAt: Date;
+  EndsAt: Date;
+  Tags: string[];
+  Photos?: Photo[];
+  PlanItems?: PlanItem[];
+}
+
+export interface Photo {
+  Id: number;
+  Name?: string;
+}
+
+export interface PlanItem {
+  PlanItemId: number;
+  ConfirmationCode?: string;
+  StartsAt?: Date;
+  EndsAt?: Date;
+  Duration?: string;
+}
+
+export enum PersonGender {
+  Male = 0,
+  Female = 1,
+  Unknown = 2
 }
